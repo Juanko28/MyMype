2a51c85fd889517ab1838a53640ba644952cdc4a
Juanko28 <juankiea.02.28@gmail.com>
2025-04-28 16:21:56 -0500

    Ejecutable

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
+const { exec } = require('child_process');
+const path = require('path'); // Para manejar rutas de manera más flexible
 
 let mainWindow;
 
 app.whenReady().then(() => {
+    // Función para iniciar el backend de Python
+    function startBackend() {
+        const backendPath = path.join(__dirname, 'Backend', 'app.py');  // Ruta absoluta a app.py
+        exec(`python "${backendPath}"`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error ejecutando el backend: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.error(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+        });
+    }
+
+    // Iniciar el backend cuando la aplicación esté lista
+    startBackend();
+
     mainWindow = new BrowserWindow({
         width: 1000,
         height: 1000,
